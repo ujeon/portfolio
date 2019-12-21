e9d8e8b90651624fc8603f45d8a9b5c4fb19661d
Ujeon <wj9304@gmail.com>
2019-12-21 20:09:09 +0900

    :bug: Set public url to fix 404 error
    404에러 해결을 위해 퍼블릭 u

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
 import Main from "./views/Main";
 import AboutMe from "./views/AboutMe";
 
@@ -8,8 +7,12 @@ const App: React.FC = () => {
   return (
     <div>
       <Switch>
-        <Route exact path="/portfolio/" component={Main} />
-        <Route exact path="/portfolio/about-me" component={AboutMe} />
+        <Route exact path={process.env.PUBLIC_URL + "/"} component={Main} />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/about-me"}
+          component={AboutMe}
+        />
       </Switch>
     </div>
   );
--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -11,9 +11,9 @@ import {
 const Main: React.FC = () => {
   return (
     <MainContainer>
-      <MainTitle>환영합니다!</MainTitle>
+      <MainTitle>환영합니다</MainTitle>
       <MainBottom>
-        <StyledLink to="/portfolio/about-me">
+        <StyledLink to="/about-me">
           <Button>더 알아보기</Button>
         </StyledLink>
       </MainBottom>
