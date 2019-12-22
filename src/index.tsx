d5fbf55c8dfb903b93a565123a206a0124fb4128
Ujeon <wj9304@gmail.com>
2019-12-22 21:39:14 +0900

    :bug: Use Hashrotuer for fixing 404 error
    4040 에러 해결을 위한 해쉬 라우터 사용

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import "./css/reset.css";
 require("dotenv").config();
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
