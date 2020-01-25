90cdc060cae0762618fdeb8a80214ec561c28dde
Ujeon <wj9304@gmail.com>
2020-01-25 11:24:49 +0900

    :bug: Fix google analytics error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+
+import { Switch, Route, useHistory } from "react-router-dom";
+import ReactGA from "react-ga";
+
 import Main from "./views/Main";
 import AboutMe from "./views/AboutMe";
 
+ReactGA.initialize("UA-156912055-1");
+ReactGA.pageview("/");
+ReactGA.pageview("/about-me");
+
 const App: React.FC = () => {
+  let history = useHistory();
+  history.listen(location => ReactGA.pageview(location.pathname));
+
   return (
     <div>
       <Switch>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
+
 import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import "./css/reset.css";
 require("dotenv").config();
+
+ReactGA.initialize("UA-156912055-1");
 
 ReactDOM.render(
   <HashRouter basename={process.env.PUBLIC_URL}>
