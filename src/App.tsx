360b391946e31f2f2914a11c3acf9478bb278d81
Ujeon <wj9304@gmail.com>
2020-02-12 21:53:14 +0900

    :sparkles: Add react helmet for SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import { Switch, Route } from "react-router-dom";
 import ReactGA from "react-ga";
@@ -11,11 +12,23 @@ ReactGA.initialize("UA-156912055-1");
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/about-me" component={AboutMe} />
-      <Route exact path="/:project" component={ProjectDetail} />
-    </Switch>
+    <>
+      <Helmet>
+        <title>우전 - 포트폴리오</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="안녕하세요. 개발을 통해 가치를 전달하고 싶은 개발자 장우전의 포트폴리오입니다. 😚"
+        />
+        <meta name="keywords" content="우전, 장우전, ujeon, jangujeon" />
+        <meta name="author" content="Ujeon" />
+      </Helmet>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/about-me" component={AboutMe} />
+        <Route exact path="/:project" component={ProjectDetail} />
+      </Switch>
+    </>
   );
 };
 
