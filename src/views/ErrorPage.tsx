207714f8a2df17a122c5638e5d1e7ca9f4c84583
Ujeon <wj9304@gmail.com>
2020-02-21 22:17:40 +0900

    :bug: Fix 404 error page

--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -7,6 +7,7 @@ const ErrorPage = () => {
     <>
       <Helmet>
         <title>404 Error</title>
+        <meta name="prerender-status-code" content="404" />
         <meta charSet="utf-8" />
       </Helmet>
       <div>페이지를 찾을 수 없습니다.</div>
