42f7ee2d23d59c2ff1ff7f6f2d964fb2c15fd28c
Ujeon <wj9304@gmail.com>
2020-02-24 09:36:53 +0900

    :bug: Try to fix navigation refresh error

--- src/styledComponents/components/_navigation.ts
+++ src/styledComponents/components/_navigation.ts
@@ -6,35 +6,28 @@ import { device } from "../../data/display";
 import * as Color from "../base/_colors";
 import * as Font from "../base/_fonts";
 
-import { LeftToRight } from "../base/_animation";
+import { ShowUnderline } from "../base/_animation";
 
 export const Navigation = styled.nav`
   width: 100%;
-  height: 7vh;
+  height: 5.3vh;
   background-color: #fff;
-  opacity: 0.95;
+  opacity: 0.99;
 
   position: fixed;
   display: flex;
   justify-content: space-between;
   align-items: center;
-
   z-index: 10;
-
-  padding: 0% 2%;
+  padding: 0% 13%;
 
   @media only screen and ${device.desktop} {
-    height: 6vh;
-  }
-
-  @media only screen and ${device.tabletLandscape} {
-    height: 8vh;
-    padding: 0% 5%;
+    height: 4.3vh;
   }
 `;
 
 export const BackButton = styled.i`
-  font-size: 3.5rem;
+  font-size: 2.7rem;
   color: ${Color.color_black};
   cursor: pointer;
 `;
@@ -55,9 +48,9 @@ export const TextButton = styled(Link)`
   flex-direction: column;
   box-sizing: border-box;
   color: ${Color.color_black};
-  font-size: 1.7rem;
+  font-size: 1.3rem;
   font-family: ${Font.font_primary};
-  font-weight: 500;
+  font-weight: 300;
   text-decoration: none;
   position: relative;
 
@@ -67,7 +60,7 @@ export const TextButton = styled(Link)`
 
   &:hover {
     ${TextUnderLine} {
-      animation: 0.7s ${LeftToRight} forwards;
+      animation: 0.7s ${ShowUnderline} forwards;
     }
   }
 
