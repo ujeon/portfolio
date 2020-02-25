499f01d241511035615a928cad3768e32377f02a
Ujeon <wj9304@gmail.com>
2020-02-25 16:59:44 +0900

    :art: Structured data test

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -31,7 +31,7 @@ const Main: React.FC<RouteComponentProps> = ({
         title="안녕하세요. 개발을 통해 가치를 전달하고 싶은 개발자 장우전입니다."
         description="마케팅을 공부한 개발자로써, 사용자의 입장에서 코드를 작성하는 개발자 장우전이 되고 싶습니다. JavaScript, React, Node, CSS, HTML 등에 관심이 있습니다."
       ></CustomHelmet>
-      <main itemScope itemType="https://schema.org/Person">
+      <main itemScope itemType="https://schema.org/CreativeWork">
         <NavBar pathname={location.pathname} />
 
         <HeadLine text={description} schemaInfo={schemaInfo} />
